1a6577184738069e3a104f21f5d49bbfc08fcdbf
Christoph Hößle <christoph_steinwachs@yahoo.de>
2014-02-13 17:40:05 +0100

    Removed folder Contacts

--- Contacts/server/config.js
+++
@@ -1,9 +0,0 @@
-path = require('path');
-
-module.exports = {
-  server: {
-    listenPort: 4000,                                       // The port on which the server is to listen (means that the app is at http://localhost:4000 for instance)
-    distFolder: path.resolve(__dirname, '../client/dist'),   // The folder that contains the application files
-    staticUrl: '/static/'                                    // The base url from which we serve static files (such as js, css and images)
-  }
-};
